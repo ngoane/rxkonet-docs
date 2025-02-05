b50949cf6540262874b2de7f673f9082e967c7a8
ehoneahobed <ehoneahobed@hotmail.com>
2025-02-05 11:28:19 -0500

    remove custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,9 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.rxkonet.com',
+  url: 'https://ngoane.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/rxkonet-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
